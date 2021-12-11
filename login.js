46bc52000355d4aaf0198a478025c92cb2b9f2f6
nisse26a <Efraim.Bring@gmail.com>
2021-12-11 12:42:01 +0300

    1.0.9

--- login.js
+++ login.js
@@ -1,15 +1,4 @@
-object.onclick = function() { myFunction };  
 
-function myFunction() {
-    var x = document.getElementById('ul');
-         if (x.style.visibility === 'visible') {
-            x.style.visibility = 'hidden';
-        } else {
-    x.style.visibility = 'visible';
-  }
-    var element = document.getElementById("nav-icon");
-    element.classList.toggle("open");
-}
 
 function validate() {
     var username = document.getElementById("username").value;
